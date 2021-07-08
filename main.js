c360a3ee3984a7da8c1adca1a3d265559c0eb90f
AalaaAlDameh <aalaaaldameh@gmail.com>
2021-07-08 15:04:13 +0300

    Meaningful Message

---
+++ main.js
@@ -0,0 +1,98 @@
+console.log(arrowTitle)
+/* START CODE UNDER THIS LINE */
+
+// ======= Question 1==========
+console.log(1+1)
+9 + 1 * 5; // 14
+console.log(9 + 1 * 5) 
+9 + 1 * 5 
+
+9 % 2 // 1
+console.log(9 % 2)
+console.log ((9 / 3) * (10 - 7)) // 9
+21 / 3 + 3 * 9 * 9 + 5 // 225
+36 / 9 + 48 - 10 / 2 //47
+
+//====== question 2
+const name = 'John';
+const age = 10;
+const const12 = "Constant";
+const variable1 = "value";
+
+//============= question 3
+let color="yellow"
+let positiviveNumber=7
+let food="sheeshbrk"
+let phonNumber="0789549203"
+
+//============= question 4
+10 + "10"; //"1010"
+"10" + "10"; // "1010"
+10 + 10 + "5"; // "205"
+"Hello" + 1; // "Hello1"
+"10" + 5 + 5; // "1055"
+"20" - 10; // 10
+"9" - "5"; // 4
+"19" - "13" + 17; //     
+5 + 6 + "4" + 9 - 4 - 2; //1143
+
+
+//=============question5
+
+
+
+
+//=============question6
+
+
+
+
+
+//=============question7
+
+
+
+
+//=============question8
+
+
+//=============question9
+
+
+
+
+//=============question10
+
+
+
+
+//=============question11
+
+
+
+
+
+//=============question12
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
